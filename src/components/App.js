0581ecc0a8c28f5077ecf80f82b7f63245205983
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-28 06:07:50 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
 
 import React from "react";
+import Modal from "./../Modal";
 import './../styles/App.css';
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* <h1>Modal Example</h1> */}
+      <button onClick={() => setShowModal(true)}>Show Modal</button>
+      <Modal show={showModal} onClose={handleModalClose}>
+        {/* <h2>Modal Content</h2> */}
+        <p>This is the content inside the modal.</p>
+      </Modal>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
